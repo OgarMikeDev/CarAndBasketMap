f06467bbccdad816a8dd8399acbd85a1aa558945
Mike Ogar <ogar.m.v@mail.ru>
2023-04-26 07:23:58 +0300

    "work Map"

---
+++ src/main/java/org/example/record/Main.java
@@ -0,0 +1,8 @@
+package org.example.record;
+
+public class Main {
+    public static void main(String[] args) {
+        Student studentOne = new Student("Ivan", 17);
+        System.out.println(studentOne.createAdultStudent("Masha"));
+    }
+}
