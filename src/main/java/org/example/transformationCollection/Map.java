85fcbfe37828064f43b3660b7ff077bfa1ad2898
Mike Ogar <ogar.m.v@mail.ru>
2023-04-26 12:35:04 +0300

    "work Map"

--- src/main/java/org/example/transformationCollection/Map.java
+++ src/main/java/org/example/transformationCollection/Map.java
@@ -1,13 +1,20 @@
 package org.example.transformationCollection;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Map {
     public static void main(String[] args) {
-        HashMap<String, Integer> basket = new HashMap<>();
-        basket.put("Milk", 5);
-        basket.put("Cottage cheese", 4);
-        basket.put("Coffee", 1);
+        HashMap<String, Integer> basketHashMap = new HashMap<>();
+        basketHashMap.put("Milk", 5);
+        basketHashMap.put("Cottage cheese", 4);
+        basketHashMap.put("Coffee", 1);
 
+
+        TreeMap<String, Integer> basketTreeMap = new TreeMap<>(new ProductComparator());
+//        basketTreeMap.putAll(basketHashMap);
+        for (java.util.Map.Entry entry : basketHashMap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
     }
 }
