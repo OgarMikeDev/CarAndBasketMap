467bd334495664dac6f627458cf1d673564b06bf
Mike Ogar <ogar.m.v@mail.ru>
2023-04-26 11:53:41 +0300

    "work Map"

--- src/main/java/org/example/transformationCollection/Map.java
+++ src/main/java/org/example/transformationCollection/Map.java
@@ -1,4 +1,13 @@
 package org.example.transformationCollection;
 
+import java.util.HashMap;
+
 public class Map {
+    public static void main(String[] args) {
+        HashMap<String, Integer> basket = new HashMap<>();
+        basket.put("Milk", 5);
+        basket.put("Cottage cheese", 4);
+        basket.put("Coffee", 1);
+
+    }
 }
