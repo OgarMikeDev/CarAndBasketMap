43056da307d572c7d273f73441becb4a80b29d03
Mike Ogar <ogar.m.v@mail.ru>
2023-04-28 09:38:59 +0300

    "work Map"

--- src/main/java/Collection/Main.java
+++ src/main/java/Collection/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Game game = new Game();
         game.initPlayers();
         System.out.println("List full players: " + game.getListPlayers());
-        game.add("Elena Muraveva");
+        System.out.println("\nPlayes added: " + game.add("Elena Muraveva"));
         System.out.println("\nList full winners: " + game.getWinners());
         System.out.println("\nList full players: " + game.getListPlayers());
     }
