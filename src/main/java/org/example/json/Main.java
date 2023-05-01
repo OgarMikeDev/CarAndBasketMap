2bc2558468255602bb740921fddae96b6b5f0428
Mike Ogar <ogar.m.v@mail.ru>
2023-05-01 18:28:37 +0300

    "work Map"

--- src/main/java/org/example/json/Main.java
+++ src/main/java/org/example/json/Main.java
@@ -3,9 +3,12 @@ package org.example.json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,6 +20,14 @@ public class Main {
         Person person = objectMapper.readValue(json, Person.class);
 
 
-        System.out.println(person.getName());
+        person.setName("Alex");
+
+
+
+        String newJson = objectMapper.writeValueAsString(person);
+
+        FileWriter fileWriter = new FileWriter("personModified.json");
+        fileWriter.write(newJson);
+        fileWriter.close();
     }
 }
